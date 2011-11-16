68c1302225d95903bec1cc0e9574f8af17be64f1
Stefan Hoth <sh@jnamic.com>
2011-11-16 20:27:27 +0100

    Removed smooth scroll (already integrated in Website.js)

--- js/combine.sh
+++ js/combine.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Combining all javascript resources into one file in the correct order..."
-cat jquery.min.js WebSite.js ImageParticle.js scroll.js  schedule.js GDD2011Berlin.js > all.js
+cat jquery.min.js WebSite.js ImageParticle.js schedule.js GDD2011Berlin.js > all.js
 echo "Done..."
